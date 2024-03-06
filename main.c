497e8c4017319b97232de33fdf4e17f0ccb2af06
Ido0302 <160231274+Ido0302@users.noreply.github.com>
2024-03-06 18:36:22 +0200

    Update main.c

--- main.c
+++ main.c
@@ -3,34 +3,41 @@
 
 int main(){
 int a,b;
-printf("Please enter two numbers: \n");
 scanf("%d", &a);
 scanf("%d", &b);
 int i;
-printf("The prime numbers between %d to %d are: ", a,b);
+printf("The Armstrong numbers are:");
 for ( i = a; i <= b; i++){
-    if(isPrime(i))
-        printf("%d, ", i);
-  }
+    if(isArmstrong(i)){
+        printf(" ");
+        printf("%d",i);
+    }
+}
 printf("\n");
-printf("The armstrong numbers between %d to %d are: ", a,b);
+printf("The Palindromes are:");
 for ( i = a; i <= b; i++){
-    if(isArmstrong(i))
-        printf("%d, ", i);
+    if(isPalindrome(i)){
+        printf(" ");
+        printf("%d",i);
     }
+}
 printf("\n");
-printf("The strong numbers between %d to %d are: ", a,b);
- for ( i = a; i <= b; i++){
-    if(isStrong(i)) 
-        printf("%d, " ,i);
+printf("The Prime numbers are:");
+for ( i = a; i <= b; i++){
+    if(isPrime(i)){
+        printf(" ");
+        printf("%d",i);
     }
- printf("\n");
- printf("The palindrom numbers between %d to %d are: " ,a,b);
- for ( i = a; i <= b; i++){
-    if(isPalindrome(i))
-        printf("%d, " ,i);
-         }
- printf("\n");
+}
+printf("\n");
+printf("The Strong numbers are:");
+for ( i = a; i <= b; i++){
+    if(isStrong(i)){
+        printf(" ");
+        printf("%d",i);
+    }
+}
+printf("\n");
 
     return 0;
 }
